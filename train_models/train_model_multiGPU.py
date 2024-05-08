bc00c3baa508c4de1915adb8aa1f437e8dddde3e
AIS AI <ais.ai@aistorm.ai>
2024-05-08 11:40:36 -0400

    init6

--- train_models/train_model_multiGPU.py
+++ train_models/train_model_multiGPU.py
@@ -3,6 +3,7 @@ Distributed training across multiple devices (GPUs)
 Model: MTCNN
 """
 import os
+import json
 import math
 import tensorflow as tf
 from MTCNN_config import *
@@ -29,18 +30,32 @@ class Train:
                 print(">>>> num_replicas_in_sync: %d, batch_size: %d" % (strategy.num_replicas_in_sync, self.batch_size))
             self.base_dir   = config.BASE_DIR
             self.lr_base    = config.LR_BASE
-            self.reduce_lr_callback = self.__my_callback()
+            # self.reduce_lr_callback = self.__my_callback()
             self.optimizer  = self.__init_optimizer()
             self.dataset    = self.__load_dataset(mode='train')
             self.num_batches= self.__define_batches()
             self.classification_accuracy_metric = tf.keras.metrics.SparseCategoricalAccuracy()
             self.bbox_accuracy_metric = tf.keras.metrics.MeanSquaredError()
+            self.landmark_accuracy_metric = tf.keras.metrics.MeanSquaredError()
+
             self.checkpoint_path = "checkpoints/"
             if not os.path.exists(self.checkpoint_path):
                 os.makedirs(self.checkpoint_path)
             self.checkpoint_prefix  = os.path.join(self.checkpoint_path, f"ckpt_{self.net}_")
             self.checkpoint         = tf.train.Checkpoint(optimizer=self.optimizer, model=self.model)
-
+            self.best_checkpoint_path = os.path.join(self.checkpoint_path, f"best_{self.net}")
+            self.best_checkpoint = tf.train.Checkpoint(optimizer=self.optimizer, model=self.model)
+            self.best_loss = float("inf")
+            self.history = {
+                "epochs": [],
+                "loss": [],
+                "classification_accuracy": [],
+                "bbox_accuracy": [],
+                "landmark_accuracy": [],
+                "learning_rate": []
+            }
+            self.history_file = os.path.join(self.checkpoint_path, "training_history.json")
+            
             self.__train_step = tf.function(self.__train_step)
 
     def __load_dataset(self,mode):
@@ -74,23 +89,17 @@ class Train:
         optimizer = tf.keras.optimizers.experimental.SGD(learning_rate = self.lr_base, momentum=0.9)
         return optimizer
     
-    def __my_callback(self,):
-        reduce_lr_callback = tf.keras.callbacks.ReduceLROnPlateau(
-                                monitor='loss', 
-                                factor=0.1,  
-                                patience=3,  
-                                verbose=1,  
-                                min_lr=1e-6  
-                            )
-        return reduce_lr_callback
+    def __reduce_lr_on_plateau(self, epoch_loss, factor=0.1, patience=3, min_lr=1e-6):
+        if self.lr_base > min_lr and epoch_loss >= self.best_loss:
+            patience -= 1
+            if patience == 0:
+                self.lr_base = max(self.lr_base * factor, min_lr)
+                self.optimizer.learning_rate.assign(self.lr_base)
+                patience = 3
+                print(f"Reducing learning rate to {self.lr_base}")
+        elif epoch_loss < self.best_loss:
+            self.best_loss = epoch_loss
     
-    # def apply_gradients_with_sparse_handling(self, gradients, variables, optimizer):
-    # # Convert gradients to dense only where necessary
-    #     def convert_to_dense(g):
-    #         return g if not isinstance(g, tf.IndexedSlices) else tf.convert_to_tensor(g)
-        
-    #     gradients = [convert_to_dense(g) for g in gradients]
-    #     optimizer.apply_gradients(zip(gradients, variables))
     def __convert_sparse_to_dense(self, gradients):
         """Convert sparse gradients to dense for efficient computation."""
         return [
@@ -116,14 +125,13 @@ class Train:
             # Update metrics
             self.classification_accuracy_metric.update_state(labels, classifier_output)
             self.bbox_accuracy_metric.update_state(rois, bbox_output)
+            self.landmark_accuracy_metric.update_state(landmarks, landmark_output)
 
             total_loss = (loss_classifier + loss_bbox + loss_landmark)
 
         gradients = tape.gradient(total_loss, self.model.trainable_variables)
         gradients = self.__convert_sparse_to_dense(gradients)
         optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
-
-        # self.apply_gradients_with_sparse_handling(gradients, self.model.trainable_variables, optimizer)
     
         return total_loss, loss_classifier, loss_bbox, loss_landmark
 
@@ -150,7 +158,13 @@ class Train:
         print(f'\r{prefix}/{total} |{bar}| {percent}% Loss: {loss:.4f} Classification Accuracy: {self.classification_accuracy_metric.result():.4f} {suffix}', end='\r')
         if iteration == total:
             print()
-    
+
+    def __save_history_to_json(self):
+        """Save training history to a JSON file."""
+        with open(self.history_file, 'w') as f:
+            json.dump(self.history, f, indent=4)
+            print('Saving training history...')
+
     def train(self, num_epochs):
         for epoch in range(num_epochs):
             print(f'Epoch {epoch + 1}/{num_epochs}')
@@ -158,6 +172,7 @@ class Train:
             num_processed_batches = 0
             epoch_classification_accuracy = 0
             epoch_bbox_accuracy = 0
+            epoch_landmark_accuracy = 0
             for i, (images, labels, rois, landmarks) in enumerate(self.dataset):
                 # loss_values, _, _, _ = self.__train_step(images=images, labels=labels, rois=rois, landmarks=landmarks, 
                 #                         optimizer= self.optimizer)  
@@ -167,6 +182,8 @@ class Train:
                 epoch_loss += current_loss
                 epoch_classification_accuracy += self.classification_accuracy_metric.result()
                 epoch_bbox_accuracy += self.bbox_accuracy_metric.result()
+                epoch_landmark_accuracy += self.landmark_accuracy_metric.result()
+
                 num_processed_batches += 1
 
                 self.__print_progress(i + 1, self.num_batches, current_loss, prefix=i, suffix='', length=20)
@@ -177,13 +194,39 @@ class Train:
             average_epoch_loss = epoch_loss / num_processed_batches
             average_epoch_classification_accuracy = epoch_classification_accuracy / num_processed_batches
             average_epoch_bbox_accuracy = epoch_bbox_accuracy / num_processed_batches
-            self.reduce_lr_callback.on_epoch_end(epoch, logs={'loss': average_epoch_loss})
+            average_epoch_landmark_accuracy = epoch_landmark_accuracy / num_processed_batches
+
+            self.__reduce_lr_on_plateau(average_epoch_loss, factor=0.1, patience=3)
+
+            # Save model weights in h5 format
+            h5_checkpoint_prefix = self.checkpoint_prefix + f"{epoch + 1}.h5"
+            self.model.save_weights(h5_checkpoint_prefix)
+            # Save checkpoint with optimizer
+            self.checkpoint.save(file_prefix=self.checkpoint_prefix)
+
 
             print(f'/nEpoch {epoch+1}/naverage_epoch_loss: {average_epoch_loss:.4f}/n\
                   average_epoch_classification_accuracy: {average_epoch_classification_accuracy}/n\
-                  average_epoch_bbox_accuracy: {average_epoch_bbox_accuracy}')
+                  average_epoch_bbox_accuracy: {average_epoch_bbox_accuracy}/n\
+                   average_epoch_landmark_accuracy: {average_epoch_landmark_accuracy} ')
             
-            self.checkpoint.save(file_prefix=self.checkpoint_prefix + f"epoch_{epoch+1}")
+            # self.checkpoint.save(file_prefix=self.checkpoint_prefix + f"epoch_{epoch+1}")
+            # Check if this is the best model
+            if average_epoch_loss < self.best_loss:
+                self.best_loss = average_epoch_loss
+                self.model.save_weights(self.best_checkpoint_path + ".h5")
+                self.best_checkpoint.save(file_prefix=self.best_checkpoint_path)
+                print(f"\nNew best model saved with loss: {self.best_loss:.4f}")
+            
+            # Update history and save to JSON
+            self.history["epochs"].append(epoch + 1)
+            self.history["loss"].append(average_epoch_loss.numpy().tolist())
+            self.history["classification_accuracy"].append(average_epoch_classification_accuracy.numpy().tolist())
+            self.history["bbox_accuracy"].append(average_epoch_bbox_accuracy.numpy().tolist())
+            self.history["landmark_accuracy"].append(average_epoch_landmark_accuracy.numpy().tolist())
+            self.history["learning_rate"].append(self.lr_base)
+
+            self.__save_history_to_json()
 
         print("Training complete!")
 
